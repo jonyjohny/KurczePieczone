53e334accd205aeda3f8aeaa3b12e1e0ec36da8c
Jonatan Taterski <jonatan.taterski@gmail.com>
2022-10-31 09:37:10 +0100

    KP21 - fix vie

--- resources/views/breeding/index.blade.php
+++ resources/views/breeding/index.blade.php
@@ -1,16 +1,19 @@
 <x-app-layout>
     <x-slot name="header">
-        <h2 class="font-samibold text-xl text-gray-800 leading-tight">
+        <h2 class="font-semibold text-xl text-gray-800 leading-tight">
             {{ __('translations.navigation.Breeding') }}
         </h2>
     </x-slot>
+
     <div class="py-12">
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
-            <div class="bg-white overflow-hidden shadow-xl sm:rounded-lg" id="table-view-wrapper">
+            <div class="bg-white shadow-xl sm:rounded-lg" id="table-view-wrapper">
                 <div class="grid justify-items-stretch pt-2 pr-2">
+                    @can('create', App\Models\Breeding::class)
                     <x-button primary label="{{ __('translations.actions.create')}}"
                         href="{{ route('breeding.create')}}"  class="justify-self-end" />
-                </div>
+                        @endcan
+                    </div>
                 <livewire:breeding.breeding-table-view />
             </div>
         </div>
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -1,50 +1,52 @@
 <!DOCTYPE html>
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
-    <head>
-        <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
-        <meta name="csrf-token" content="{{ csrf_token() }}">
 
-        <title>{{ config('app.name', 'Laravel') }}</title>
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="csrf-token" content="{{ csrf_token() }}">
 
-        <!-- Fonts -->
-        <link rel="stylesheet" href="https://fonts.bunny.net/css2?family=Nunito:wght@400;600;700&display=swap">
+    <title>{{ config('app.name', 'Laravel') }}</title>
 
-        <!-- Styles -->
-        @livewireStyles
-        @laravelViewsStyles('laravel-views')
+    <!-- Fonts -->
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Nunito:wght@400;600;700&display=swap">
 
-        <!-- Scripts -->
-        @wireUiScripts
-        @vite(['resources/css/app.css', 'resources/js/app.js'])
-        
-    </head>
-    <body class="font-sans antialiased">
-        <x-jet-banner />
-        <x-notifications />
-        <x-dialog />
+    <!-- Styles -->
+    @livewireStyles
+    @laravelViewsStyles(laravel-views)
 
-        <div class="min-h-screen bg-gray-100">
-            @livewire('navigation-menu')
+    <!-- Scripts -->
+    @wireUiScripts
+    @vite(['resources/css/app.css', 'resources/js/app.js'])
+</head>
 
-            <!-- Page Heading -->
-            @if (isset($header))
-                <header class="bg-white shadow">
-                    <div class="max-w-7xl mx-auto py-6 px-4 sm:px-6 lg:px-8">
-                        {{ $header }}
-                    </div>
-                </header>
-            @endif
+<body class="font-sans antialiased">
+    <x-jet-banner />
+    <x-notifications />
+    <x-dialog />
 
-            <!-- Page Content -->
-            <main>
-                {{ $slot }}
-            </main>
-        </div>
+    <div class="min-h-screen bg-gray-100">
+        @livewire('navigation-menu')
 
-        @stack('modals')
+        <!-- Page Heading -->
+        @if (isset($header))
+        <header class="bg-white shadow">
+            <div class="max-w-7xl mx-auto py-6 px-4 sm:px-6 lg:px-8">
+                {{ $header }}
+            </div>
+        </header>
+        @endif
 
-        @livewireScripts
-        @laravelViewsScripts('laravel-views')
-    </body>
+        <!-- Page Content -->
+        <main>
+            {{ $slot }}
+        </main>
+    </div>
+
+    @stack('modals')
+
+    @livewireScripts
+    @laravelViewsScripts(laravel-views)
+</body>
+
 </html>
